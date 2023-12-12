5d7c9ffcef1a91f7bbe8a3ef215d4201064d9963
ElopingDev <elopingbuisness@gmail.com>
2023-12-12 01:42:08 +0100

    added rapidfire

--- src/globals.h
+++ src/globals.h
@@ -5,6 +5,7 @@
 namespace globals {
 	DWORD baseAddress = 0x057E0A8;
 	DWORD recoilAddress = 0x04C2EC3;
+	DWORD rapidfireAddress = 0x04C727D;
 	std::vector<DWORD> ammoOffsets{ 0x364, 0x14, 0x0 };
 	std::vector<DWORD> healthOffsets{ 0xEC };
 }
--- src/main.cpp
+++ src/main.cpp
@@ -15,22 +15,35 @@ int main(int argc, char** argv)
 	GetWindowThreadProcessId(acWindow, &pid);
 	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, true, pid);
 	std::vector<BYTE> originalInstructions(5);
-	bool setOnce = false;
+	std::vector<BYTE> rapidfireOriginalInstructions(5);
+
+	bool recoilSetOnce = false;
+	bool rapidFireSetOnce = false;
+
 
 
 	while (!GetAsyncKeyState(VK_END)) {
 	ModifyProcessMemory(hProcess, globals::baseAddress, globals::ammoOffsets, 666);
 	ModifyProcessMemory(hProcess, globals::baseAddress, globals::healthOffsets, 666);
 
-	if (setOnce == false)
+	if (recoilSetOnce == false)
 	{
 		NopMemory(hProcess, (void*)globals::recoilAddress, 5, originalInstructions);
 		PrintInstructions(originalInstructions);
-		setOnce = true;
+		recoilSetOnce = true;
+	}
+
+	if (rapidFireSetOnce == false)
+	{
+		NopMemory(hProcess, (void*)globals::rapidfireAddress, 5, rapidfireOriginalInstructions);
+		PrintInstructions(rapidfireOriginalInstructions);
+		rapidFireSetOnce = true;
 	}
 
 	}
 	
 	RestoreMemory(hProcess, (void*)globals::recoilAddress, 5, originalInstructions);
+	RestoreMemory(hProcess, (void*)globals::rapidfireAddress, 5, rapidfireOriginalInstructions);
+
 	return 0;
 }
